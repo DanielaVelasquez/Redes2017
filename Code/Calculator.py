f236815ef0098c0cf10d886059095ca3617d3c08
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-08-09 16:57:32 -0500

    Actualizacion calculadora

--- Code/Calculator.py
+++ Code/Calculator.py
@@ -1,22 +1,31 @@
 import sys
+sys.path.insert(0,'Redes2017/Constants/Constants')
+##import Constants
 
 class Calculator:
 
-	def __init__(self):
-		self.primer_operando = 0
-		self.segundo_operando = 0
+	def __init__(self,first_op=0,second_op=0):
+		self.first_operand = first_op
+		self.second_operand = second_op
 
 	def add(self):
-		return self.primer_operando + self.segundo_operando
+		return self.first_operand + self.second_operand
 
 	def substract(self):
-		return self.primer_operando - self.segundo_operando
+		return self.first_operand - self.second_operand
 
 	def multiply(self):
-		return self.primer_operando * self.segundo_operando
+		return self.first_operand * self.second_operand
 
 	def divide(self):
 		try:
-			return self.primer_operando / self.segundo_operando
+			return self.first_operand / self.second_operand
 		except ZeroDivisionError as err:
-			return "No se puede dividir entre cero"
+			return "No se puede dividir entre cero"##CONS_ZERO_DIVISON_ERROR
+
+""""
+a = Calculator()
+a.first_operand = 2
+a.second_operand = 0
+print("%d" % (a.divide()))
+"""
