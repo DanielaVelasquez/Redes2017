03429210ebc40755503afc08152da51f906c5ca1
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-08-09 15:46:08 -0500

    Correcion Calculadora

--- Code/Calculator.py
+++ Code/Calculator.py
@@ -3,23 +3,20 @@ import sys
 class Calculator:
 
 	def __init__(self):
-		primer_operando = 0
-		segundo_operando = 0
+		self.primer_operando = 0
+		self.segundo_operando = 0
 
 	def add(self):
-		return primer_operando + segundo_operando
+		return self.primer_operando + self.segundo_operando
 
 	def substract(self):
-		return primer_operando - segundo_operando
+		return self.primer_operando - self.segundo_operando
 
 	def multiply(self):
-		return primer_operando * segundo_operando
+		return self.primer_operando * self.segundo_operando
 
 	def divide(self):
 		try:
-			return primer_operando / segundo_operando
+			return self.primer_operando / self.segundo_operando
 		except ZeroDivisionError as err:
 			return "No se puede dividir entre cero"
-
-
-	
