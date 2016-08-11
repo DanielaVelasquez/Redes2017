735dc1b9874ee863fbd13e4c8f34a820a457a940
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-08-10 23:09:38 -0500

    Actualizacion calculadora code, agregar usuarios

--- Code/Calculator.py
+++ Code/Calculator.py
@@ -1,6 +1,10 @@
 import sys
-sys.path.insert(0,'Redes2017/Constants/Constants')
-##import Constants
+#sys.path.insert(0,'../GUI')
+#sys.path.insert(0, "..GUI")
+
+
+FILE_NAME = "input.txt"
+SEP = " , "
 
 class Calculator:
 
@@ -23,9 +27,47 @@ class Calculator:
 		except ZeroDivisionError as err:
 			return "No se puede dividir entre cero"##CONS_ZERO_DIVISON_ERROR
 
-""""
+	def addUser(self,username,password):
+		#Revisar si esta registrado
+		if self.isRegistred(username):
+			return False
+		#Convertir password
+		newpass = self.changePassword(password)
+
+		archivo = open(FILE_NAME,'a')
+		archivo.write("\n"+username + " , "+newpass)
+		archivo.close()
+		return True
+
+	def login(self, username,password):
+		newpass = self.changePassword(password)
+		archivo = open(FILE_NAME,'r')
+		for line in archivo:
+			vals = line.split(SEP,2)
+			if(vals[0] == username and vals[1] == newpass):
+				return True
+		return False
+
+	def changePassword(self, password):
+		newpass = ""
+		for letra in password:
+			val_ascii = ord(letra)
+			val_ascii = val_ascii + 5
+			newpass = newpass + chr(val_ascii)
+		return newpass
+
+	def isRegistred(self,username):
+		archivo = open(FILE_NAME,'r')
+		for line in archivo:
+			vals = line.split(SEP,2)
+			if(vals[0] == username):
+				return True
+		return False
+
+
+
+
 a = Calculator()
 a.first_operand = 2
 a.second_operand = 0
-print("%d" % (a.divide()))
-"""
+a.addUser("root","root")
