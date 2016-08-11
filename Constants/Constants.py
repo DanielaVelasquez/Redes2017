c3891530f669e09ba3a039b6d40c43a5c586e305
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-08-11 18:46:04 -0500

    Creación constantes

--- Constants/Constants.py
+++ Constants/Constants.py
@@ -1,3 +1,18 @@
 import sys
 
 CONS_ZERO_DIVISON_ERROR = "No se puede dividir entre cero"
+CONS_ZERO_MODULE_ERROR = "Resultado indefinido"
+WIDTH_BUTTON = 64
+WIDTH_BUTTON_IMG = 32
+POINT_SYMBOL = "."
+ADD_SYMBOL = "+"
+SUBSTRACT_SYMBOL = "-"
+DIV_SYMBOL = "/"
+MUL_SYMBOL = "*"
+EQUAL_SYMBOL = "="
+IMAGE_LOCATION = "icon/add-user.png"
+INIT_ROW_NUMBERS = 3
+COLS = 3
+INVALID_OPERATION = "Operacion invalida"
+MODULE_NAME = "%"
+POT_NAME = "^"
