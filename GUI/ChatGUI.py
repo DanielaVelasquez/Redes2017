a7962a5e653cd398b8e5b39cdbb89b27b6b6b372
enriqueBC <enrique_bernal@ciencias.unam.mx>
2016-08-21 04:42:28 -0500

    Update GUI Folder
    -Añadida documentación a lo faltante
    -)ChatGUI.py
    -Al presionar "Esc" la ventana de chat se cierra
    -Presionar "Enter" para enviar mensaje no sirve :( debido a que el campo
    de texto registra el salto de linea
    -)LoginGUI.py
    -Al presionar "Esc" la ventana se cierra
    -Al presionar "Enter" se registra la informacion de los campos de texto

--- GUI/ChatGUI.py
+++ GUI/ChatGUI.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
 import sys
-from PyQt4 import QtGui
-
+from PyQt4 import QtGui, QtCore
+from Constants.Constants import *
+from Channel.Channel import Channel
 #Direcciones relativas
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-from Constants.Constants import *
-from Channel.ApiServer import FunctionWrapper
-from Channel.Channel import Channel
-
-class ChatGUI(FunctionWrapper):
+"""
+Clase de interfaz grafica que permite visualizar la conversacion con el contacto del chat
+Y enviar nuevos mensajes por medio de un campo de texto
+"""
+class ChatGUI(QtGui.QWidget):
 	
+	"""
+	Constructor
+	"""
 	def __init__(self,my_information,my_contact_information,mode):
 		super(ChatGUI, self).__init__()
 		self.my_information = my_information
@@ -24,7 +26,7 @@ class ChatGUI(FunctionWrapper):
 		self.initGUI()
 
 	"""""
-	Crea el canal de coneccion y establece la coneccion 
+	Crea el canal de conexion y establece la conexion
 	con el contacto de acuerdo a los datos entregados por
 	el usuario
 	"""
@@ -33,7 +35,7 @@ class ChatGUI(FunctionWrapper):
 			self.channel = Channel(my_port=self.my_information,contact_port=self.my_contact_information)
 		else:
 			self.channel = Channel(contact_ip = self.my_information)
-		self.channel.setWrapper(self)
+		self.channel.setWrapper(self) # Khe?.... bueno, lo dejare asi
 		self.channel.init_chat()
 
 	""""
@@ -41,58 +43,61 @@ class ChatGUI(FunctionWrapper):
 	donde se reciben los mensaje y una que permite enviar
 	los mensajes
 	"""
-	def initGUI(self):
-		#self.app = QtGui.QApplication([])
-		
+	def initGUI(self):		
 		#Creación y configuración del widget
-		self.widget = QtGui.QWidget()
-		self.widget.setWindowTitle(CHAT_WINDOW)
-		self.widget.resize(500,200)
-
+		self.setWindowTitle(CHAT_WINDOW)
+		self.setGeometry(DEFAULT_POSITION_X+10, DEFAULT_POSITION_Y+10, CHAT_WIDTH, CHAT_HEIGHT)
+		self.resize(500,200)
 		#Configuración layout
 		self.grid = QtGui.QGridLayout()
-		self.widget.setLayout(self.grid)
-
+		self.setLayout(self.grid)
 		#Declaración elementos de la GUI
-		self.lb_conversation = QtGui.QLabel(CONVERSATION_TITLE,self.widget)
-
-		self.txt_conversation = QtGui.QTextEdit(self.widget)
+		self.lb_conversation = QtGui.QLabel(CONVERSATION_TITLE,self)
+		self.txt_conversation = QtGui.QTextEdit(self)
 		self.txt_conversation.setReadOnly(True)
-		self.txt_message = QtGui.QTextEdit(self.widget)
-
-		self.btn_send = QtGui.QPushButton(SEND_TITLE,self.widget)
-		
+		self.txt_message = QtGui.QTextEdit(self)
+		self.btn_send = QtGui.QPushButton(SEND_TITLE,self)		
 		#Configuración elementos de la GUI 
 		self.grid.addWidget(self.lb_conversation,0,0)
-		self.grid.addWidget(self.txt_conversation,1,0,10,10)
-		
+		self.grid.addWidget(self.txt_conversation,1,0,10,10)		
 		self.grid.addWidget(self.txt_message,11,0,5,1)
 		self.grid.addWidget(self.btn_send,12,5,3,1)
 
-		self.btn_send.clicked.connect(self.sendMesssage)
+		self.btn_send.clicked.connect(self.sendMessage)
 
-		self.widget.show()
-		#sys.exit(self.app.exec_())
+		self.show()
 
-	"""""
-	Despliega los mensaje que se enviaron en la pantalla del chat
+	""""
+	Despliega los mensajes que se enviaron en la pantalla del chat
 	"""
-	def showSendingMesssage(self,message):
+	def showSendingMessage(self,message):
 		self.txt_message.clear()
-		self.txt_conversation.append("Yo: message")
+		last_message = "Yo: "+message
+		self.txt_conversation.append(last_message)
 
-	"""""
+	""""
 	Envia los mensajes al contacto
 	"""
-	def sendMesssage(self):
+	def sendMessage(self):
 		message = self.txt_message.toPlainText()
 		if len(message) == 0:
-			QtGui.QMessageBox.warning(self.widget, WARNING, MISSING_MESSAGE,QtGui.QMessageBox.Ok)
+			QtGui.QMessageBox.warning(self, WARNING, MISSING_MESSAGE,QtGui.QMessageBox.Ok)
 		else:
 			if not self.channel.send_text(message):
-				QtGui.QMessageBox.warning(self.widget, WARNING, CONECTION_FAIL,QtGui.QMessageBox.Ok)
+				QtGui.QMessageBox.warning(self, WARNING, CONECTION_FAIL,QtGui.QMessageBox.Ok)
 			else:
-				self.showSendingssMesssage(message)
+				self.showSendingMessage(message)
+	"""
+	Wrapper
+	"""
 	def sendMessage_wrapper(self, message):
 		self.txt_conversation.append("Contacto dice: "+message)
-		
+
+	"""
+	Define los eventos que ocurriran cuando se presionen teclas del teclado
+	"""
+	def keyPressEvent(self, event):
+		if event.key() == QtCore.Qt.Key_Escape:
+			self.close()
+		if event.key() == QtCore.Qt.Key_Return:
+			self.sendMessage()
