f3f795fe192a99c2755ddccba5f4db8959fb560a
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-08-23 18:43:11 -0500

    Merge branch 'Practica1'
    # Conflicts:
    #	GUI/CalculatorGUI.py
    #	GUI/Login.py

--- Channel/RecordAudio.py
+++ Channel/RecordAudio.py
@@ -26,7 +26,7 @@ class AudioClient(object):
 	Método que se encarga de estar grabando de forma continua el audio y encolarlo.
 
 	"""	
-	def feed_queque(queque):
+	def feed_queque(self, queque):
 		#Inicia la configuración para la grabación del audio
 		self.pyaudio = pyaudio.PyAudio()
 		self.pyaudio_format = self.pyaudio.get_format_from_width(WIDTH_PYAUDIO_FORMAT)
@@ -41,12 +41,44 @@ class AudioClient(object):
 	        queque.put(data_ar)
 #####################################################
 # Clase encargada de la reproduccion del audio      #
-# entre los usuarios conectados                     #
+# que reciba de su contacto del chat                #
 # Idea tomada de repositorio github:                #
 #     https://github.com/mvilchis/redes-17_Ejemplos #
 #####################################################
+
+from cStringIO import StringIO
+from numpy.lib import format
+
 class AudioServer(object):
 
 	def __init__(self, arg):
 		super(AudioServer, self).__init__()
-		
+
+	def toArray(self,s):
+	    f=StringIO(s)
+	    arr=format.read_array(f)
+	    return arr 
+
+	def playAudio(self,audio):
+	    p = pyaudio.PyAudio()
+	    FORMAT = p.get_format_from_width(2)
+	    stream = p.open(format=FORMAT,
+	                    channels=CHANNELS,
+	                    rate=RATE,
+	                    output=True,
+	                    frames_per_buffer=CHUNK)
+
+	    data = audio.data
+	    stream.write(data)
+	    stream.close()
+	    p.terminate()
+
+	def reproduce(self):
+	    while True:
+	        if len(frames) > 0:
+	            cv2.imshow('Servidor',frames.pop(0))
+	            if cv2.waitKey(1) & 0xFF==ord('q'):
+	                break
+	    cv2.destroyAllWindows()
+
+	
