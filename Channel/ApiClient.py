00e22005819a661c89858d2c70971783537d8c21
DanielaVelasquez <daniela.velasquez.g@gmail.com>
2016-09-05 20:41:19 -0500

    correcion coneccion remota

--- Channel/ApiClient.py
+++ Channel/ApiClient.py
@@ -35,7 +35,7 @@ class MyApiClient:
         print "-) Api client: "+message
         try:
             print "cliente: "+str(self.contact_ip)+str(self.contact_port)+"/"
-            self.proxy = xmlrpclib.ServerProxy(self.contact_ip+str(self.contact_port)+"/", allow_none=True)
+            self.proxy = xmlrpclib.ServerProxy(self.contact_ip+":"+str(self.contact_port)+"/", allow_none=True)
             self.proxy.sendMessage_wrapper(str(message))
             return True
         except Exception, ex:
