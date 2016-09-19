34d97e5b6ec44a834b1decd252778bacb004cd33
enriqueBC <enrique_bernal@ciencias.unam.mx>
2016-09-19 02:47:22 -0500

    Pruebas independientes organizadas

--- Prueba video xmlrpc/client.py
+++ Prueba video xmlrpc/client.py
@@ -4,22 +4,17 @@ import threading
 import multiprocessing as mp
 import cv2
 
-
-
 def feed_queue(q):
-
 	cap = cv2.VideoCapture(0)
 	while True:
-
 		ret, frame = cap.read()
+		#ret = cap.set(3, 600)
+		#ret = cap.set(4, 400)
 		if ret:
 			q.put(pickle.dumps(frame))
-
-
+	#cap.release()
 
 proxy = xmlrpclib.ServerProxy("http://localhost:5000/",allow_none = True)
-
-
 
 queue = mp.Queue()
 
@@ -27,10 +22,6 @@ p = threading.Thread(target=feed_queue, args=(queue,))
 
 p.start()
 
-
-
 while True:
-
 	d = queue.get()
-
 	proxy.reproduce_video(d)
--- Prueba video xmlrpc/server.py
+++ Prueba video xmlrpc/server.py
@@ -1,5 +1,4 @@
-#! /usr/bin/env pythonb
-
+#! /usr/bin/env python
 import xmlrpclib
 import pickle
 import cv2
@@ -7,52 +6,36 @@ import threading
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
+class Video(object):	
 
-class Video(object):
-	
 	def __init__(self):
 		super(Video, self).__init__()
 		self.r = False
 		self.frame = None
-		
-
 
 	def reproduce(self):
 		print "Iniciando hilo"
 		while self.r:
-			#print "Mostrando"
 			cv2.imshow('frame',self.frame)
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
 		cv2.destroyAllWindows()
 
 	def reproduce_video(self, video):
-
-		print "reproducing..."
-
+		print "Reproducing..."
 		self.frame = pickle.loads(video)
-
 		if not self.r:
-			print "no estaba reproduciendo"
+			print "No estaba reproduciendo"
 			self.r = True
 			print "Lanzando hilo"
 			p = threading.Thread(target=self.reproduce)
 			p.start()
-			print "lanzado"
-
-
-
-	#cap.release()
-
-
+			print "Lanzado"
 
 server = SimpleXMLRPCServer(("localhost", 5000), allow_none = True)
 
 a = Video()
-
 server.register_function(a.reproduce_video,'reproduce_video')
-
 print "running"
 
 server.serve_forever()
-
